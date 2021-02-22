557ac1c40ff8039e50f9fa32e9fe8171a29b3404
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-22 15:37:00 -0800

    add app.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import WordPage from '../../containers/WordPage';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={WordPage} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
