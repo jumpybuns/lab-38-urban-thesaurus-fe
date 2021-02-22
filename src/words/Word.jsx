ba2262c594bef6f999fde5db6bb75c09f565ce36
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-22 15:35:08 -0800

    add word

---
+++ src/words/Word.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Word = ({ word, definition, example }) => (
+  <figure>
+    <h1>{word}</h1>
+    <h2>{definition}</h2>
+    <h3>{example}</h3>
+  </figure>
+);
+
+Word.propTypes = {
+  id: PropTypes.string.isRequired,
+  word: PropTypes.string.isRequired,
+  definition: PropTypes.string.isRequired,
+  example: PropTypes.string.isRequired,
+};
+
+export default Word;
